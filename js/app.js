658d5fc5dcecfb46c2b1cbe2c15d2d92f64e6d09
iballi2017 <iballi2017@gmail.com>
2020-02-01 23:21:04 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+    // alert("hello");
+
+    $(".hero-text-slider").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
+    $(".pme-carousel").owlCarousel({
+        loop:true,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        fluidSpeed: 1,
+        // slideSpeed : 10000,
+        autoplaySpeed: 3000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
+    $(".customers-reviews-slider").owlCarousel({
+        loop:true,
+        autoplay: false,
+        dots: true,
+        autoplayTimeout: 10000,
+        fluidSpeed: 1,
+        // slideSpeed : 10000,
+        autoplaySpeed: 3000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+})
