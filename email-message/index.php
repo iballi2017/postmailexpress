eabdccb9b05ee0fc7ae57f8c0c7224c5634d9072
iballi2017 <iballi2017@gmail.com>
2020-02-12 14:31:17 +0100

    11/02/2020

---
+++ email-message/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Email Message</title>
+
+
+
+    <style>
+    .action-btn {
+        background-color: #2d882d;
+    }
+
+    .action-btn:hover {
+        background-color: #116611;
+    }
+    </style>
+</head>
+
+
+
+<body style="font-family: 'Roboto', sans-serif;">
+
+    <main class="wrapper" style="
+        width: 70%;
+        margin: 0 auto;
+        display: flex;
+        justify-content: center;">
+
+        <div class="main-inner" style="
+            width: 45%; line-height: 1.6">
+
+            <table style="
+                width: 100%;">
+                <thead>
+
+                    <tr>
+                        <th colspan="3">
+                            <div class="head-inner" style="
+                                height: 80px;
+                                display: flex;
+                                justify-content: center;
+                                align-items: center;">
+                                <img src="./PostMailExpress-logo.png" class="brand-logo" alt="" style="
+                                width: 150px;">
+                            </div>
+                        </th>
+                    </tr>
+
+                </thead>
+                <tbody>
+                    <tr>
+                        <td colspan="3">
+                            <div class="content" style="
+                                text-align: center;
+                                background-color: rgba(136, 204, 136, .1); padding: 3em 1.5em">
+                                <h3> Lorem, ipsum dolor. </h3>
+                                <p>
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur quae culpa iste
+                                    atque distinctio necessitatibus a animi nemo est quis.
+                                </p>
+                                <p>
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur quae culpa iste
+                                    atque distinctio necessitatibus a animi nemo est quis.
+                                </p>
+                                <p>
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur quae culpa iste
+                                    atque distinctio necessitatibus a animi nemo est quis.
+                                </p>
+
+
+                                <a href="http://" class="action-btn"
+                                    style="text-decoration: none; color: #ffffff; padding: .8em; margin-top: 2em; display: block">Click
+                                    Here To Activate
+                                    Your Account</a>
+
+
+
+                            </div>
+                            <p style=><small>&copy; 2020 postmailexpress</small></p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </main>
+
+</body>
+
+</html>
--- user/successful-payment-page.php
+++ user/successful-payment-page.php
@@ -3,13 +3,13 @@ include_once '../includes/header.php'; ?>
 
 
 
-<main class="about-us-page mb-5">
+<main class="successful-payment-page mb-5">
 
     <section class="py-5">
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <div class="text-center p-5 form-global-style success-message">
+                    <div class="text-center p-5 form-global-style">
                         <h3>Thank you, Payment is well received</h3>
                         <h4>Your patronage is important to us</h4>
                         <p class="mt-5"><a href="./user-dashboard.php">Return to your dashboard</a></p>
